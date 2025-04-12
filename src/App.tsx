39d46bb9cd84caec7c4a32dc4e2598ebf39c85a6
jhansarmiento <jhancsn1@gmail.com>
2025-04-11 23:11:33 -0500

    Renderiza en el componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import { useWeather } from "./services/UseWeather";
 
-
 function App() {
+  const [weather, setWeather] = useState({
+    temp: 0,
+    humidity: 0
+  });
 
   const Weather = async () => {
-    const { main: weather } = await useWeather()
-     console.log( "Temperatura: ", weather.temp, " Humedad: ", weather.humidity,)
-  }
+    const { main: weatherData } = await useWeather();
+    console.log("Temperatura: ", weatherData.temp, " Humedad: ", weather.humidity);
+    setWeather(weatherData);
+  };
 
-  Weather()
-  
+  useEffect(() => {
+    Weather();
+  }, []);
+
   return (
     <>
-      
+      <h1 className="text-3xl font-bold text-center p-10">Weather</h1>
+      <p className="pl-10">Temperatura: {weather.temp} °K</p>
+      <p className="pl-10">Humedad: {weather.humidity}</p>
     </>
   );
 }
